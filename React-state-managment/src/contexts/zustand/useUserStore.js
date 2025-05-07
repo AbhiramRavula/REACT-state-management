e8fc93fd15586d705cfc56773e2fd4c12efdfdf1
AbhiramRavula <138038324+AbhiramRavula@users.noreply.github.com>
2025-05-07 15:01:48 +0000

    inital commit 1

--- React-state-managment/src/contexts/zustand/useUserStore.js
+++ React-state-managment/src/contexts/zustand/useUserStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 
-const useUserStore = create((set) => ({
+export const useUserStore = create((set) => ({
     user: null,
     login: (name, email) => set({ user: { name:"pedro", email:"pedro@gmail.com" } }),
     logout: () => set({ user: null }),
